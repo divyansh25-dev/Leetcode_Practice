e424506d285c11bc1ed0968884c949ff0db0d912
divyansh25-dev <divyansh25.panwar@gmail.com>
2022-06-15 17:04:26 +0530

    Group Anagrams

--- GroupAnagrams.cpp
+++ GroupAnagrams.cpp
@@ -43,6 +43,39 @@ use the sorted string as a key and keep pushing the anagramsin the map
 
 
 
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        map<map<char, int>, vector<string>> mp;
+        vector<vector<string>>ans;
+
+
+         for(auto &x : strs){
+        map<char,int>temp;
+                for(auto &y:x){
+                temp[y]++;
+                }
+
+                mp[temp].push_back(string);
+
+         }        
+
+for(auto &x : mp){
+    ans.push_back(x.second);
+}
 
 
 
+        return ans;
+        
+    }
+};
+
+
+//O(nk) Approach
+
+/*
+In this approach the sort is replaced with map we keep the map as first part
+
+*/
